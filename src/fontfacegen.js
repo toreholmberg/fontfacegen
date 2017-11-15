43a3d693652b806a1f84e584ba1eca59813f5f19
Tore Holmberg <tore.holmberg@gmail.com>
2017-11-14 20:28:15 -0500

    renables ttf processing

--- src/fontfacegen.js
+++ src/fontfacegen.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const mkdirp = require('mkdirp').sync;
 const configure = require('./configure.js');
-// const ttf = require('./ttf.js');
+const ttf = require('./ttf.js');
 const ttf2woff = require('./ttf2woff.js');
 const ttf2woff2 = require('./ttf2woff2.js');
 const stylesheets = require('./stylesheets.js');
@@ -16,7 +16,7 @@ module.exports = options => {
     mkdirp(path.dirname(config.css));
   }
 
-  // ttf(config.source, config.ttf, config.name, config); // TODO: better options handling
+  ttf(config.source, config.ttf, config.name, config); // TODO: better options handling
   ttf2woff(config.source, config.woff);
   ttf2woff2(config.source, config.woff2);
   stylesheets(config);
